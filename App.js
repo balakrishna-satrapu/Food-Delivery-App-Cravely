a7eab710d01a3bc794244f05b41712d66889820a
Balakrishna <balakrishnasatrapu@gmail.com>
2026-02-25 17:13:27 +0530

    Added React and Babel  via CDN

--- App.js
+++ App.js
@@ -1,7 +1,22 @@
-const root = document.getElementById('root');
+// const root = document.getElementById('root');
 
-const tagLine = document.createElement('h3');
+// const tagLine = document.createElement('h3');
 
-tagLine.innerText = 'Feed your Cravings';
+// tagLine.innerText = 'Feed your Cravings';
 
-root.appendChild(tagLine);
+// root.appendChild(tagLine);
+
+// Using React
+/* <div id="parent">
+    <p id="tagline">Feed your Cravings</p>
+</div> */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("p", { id: "tagline" }, "Feed your Cravings"),
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
